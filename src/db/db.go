6ec06d7a99c6771bfb155d6a63e962cf0852d07d
harlock024 <hadith024@gmail.com>
2025-05-27 18:34:00 -0600

    feat: env to render

--- src/db/db.go
+++ src/db/db.go
@@ -11,14 +11,22 @@ import (
 
 var DB *gorm.DB
 
-func InitDB() {
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Failed to connect to database:", err)
+var DB *gorm.DB
 
+func InitDB() {
+	if os.Getenv("RENDER") == "" {
+		err := godotenv.Load()
+		if err != nil {
+			log.Println("Warning: .env file not found, continuing with system environment variables")
+		}
 	}
 
 	dsn := os.Getenv("DB_URL")
+	if dsn == "" {
+		log.Fatal("Environment variable DB_URL is not set")
+	}
+
+	var err error
 	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		log.Fatal("Failed to connect to database:", err)
